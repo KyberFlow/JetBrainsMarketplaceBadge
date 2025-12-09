d043ceaa4b226836717c5449278cd3a4efc12a13
FerdiStro <ferdistr08@gmail.com>
2025-12-09 16:01:18 +0100

    Feat: generation works. Svg now is filled with stats

--- setupLocal.sh
+++ setupLocal.sh
@@ -1,7 +1,3 @@
 echo "Setup Env Variables for Local Use"
-
-export MARKETPLACE_ID="test"
-
-
-echo "ENV set: /n"
-export printenv | grep MARKETPLACE_ID
+export MARKETPLACE_ID=""
+printenv | grep MARKETPLACE_ID
---
+++ src/file_writer.rs
@@ -0,0 +1,21 @@
+use log::{error, info};
+use std::fs::File;
+use std::io::Write;
+use tokio::io::AsyncWriteExt;
+
+pub fn save_svg(content_as_string: String, output_file_path: &str) {
+    let file = File::create(output_file_path);
+
+    let fix_file = match file {
+        Err(e) => {
+            error!("couldn't create {}: {}", output_file_path, e);
+            return;
+        }
+        Ok(mut file) => file.write_all(content_as_string.as_bytes()),
+    };
+
+    match fix_file {
+        Err(e) => error!("couldn't write to {}: {}", output_file_path, e),
+        Ok(_) => info!("wrote to {}", output_file_path),
+    }
+}
